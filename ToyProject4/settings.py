08ead28af2d8a4c67ab2a463dbcd5b29e3ffaf2b
an dong woo <akskwha@naver.com>
2023-06-23 20:54:22 +0900

    메인 화면 구현(게시판)

--- ToyProject4/settings.py
+++ ToyProject4/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "test_app"
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,63 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Button } from 'react-bootstrap';
 const IndexPage = () => {
   return (
     <div>
-      <h1>Welcome to Next.js!</h1>
+          <Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>제몫</th>
+        <th>글쓴이</th>
+        <th>조회 수</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </Table>
+    <Button variant="primary">Primary</Button>
     </div>
   );
 };
+// function BasicExample() {
+//   return (
+//     <div>
+//       <Table striped bordered hover>
+//       <thead>
+//         <tr>
+//           <th>id</th>
+//           <th>제목</th>
+//           <th>글쓴이</th>
+//           <th>조회</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>1</td>
+//           <td>Mark</td>
+//           <td>Otto</td>
+//           <td>@mdo</td>
+//         </tr>
+//       </tbody>
+//     </Table>
+//     {/* 버튼 컴포넌트 */}
+//     {TypesExample()}
+//     </div>
+//   );
+// }
+// function TypesExample() {
+//   return (
+//     <>
+//       <Button variant="primary">Primary</Button>{' '}
+//     </>
+//   );
+// }
 
 export default IndexPage;
