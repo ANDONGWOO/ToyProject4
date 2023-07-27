4c1f90ead78aaebb8376ecc22ebd6c57e7ef618e
an dong woo <akskwha@naver.com>
2023-07-27 23:55:01 +0900

    gitignore수정

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6d6bf8789e107cf3"; }
+/******/ 		__webpack_require__.h = function() { return "00790c5c87e6b866"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
